20adfe83af4e24074cab4a276e04fddf8b679689
Ulises <ulisesm.ramirez@gmail.com>
2020-03-22 18:17:02 -0300

    Changed the session style to a class for adding new tables

--- MainProgram.py
+++ MainProgram.py
@@ -2,50 +2,41 @@ from internalData.userData import serviceCredentials as credentials
 from internalData.baseExpValues import base, exp
 from internalData import moneyInfo
 
-'''
-TODO:
-Thinking of adding a separated .json for every user, and
-the admin account as a validator for creating new users
-and assigning the new file to the user. Then establish a
-dataBase-like functionality to access each of the files
-separately
-'''
-def programFlow():
+def programFlow(session):
     indication = input('\nIngress the desired action: ').split()
-    if indication[0] in moneyInfo.functionsList:
+    if indication[0] in session.functionsList:
         if len(indication) > 1:
-            if indication[1] in moneyInfo.lanes:
-                action = moneyInfo.functionsList[indication[0]]
+            if indication[1] in session.lanes:
+                action = session.functionsList[indication[0]]
                 argument = indication[1]
-                action(argument)
-                programFlow()
+                action(session, argument)
+                programFlow(session)
             else:
                 print('Invalid lane given, try again. ')
-                programFlow()
+                programFlow(session)
         else:
-            action = moneyInfo.functionsList[indication[0]]
-            action()
-            programFlow()
-    elif indication[0] in moneyInfo.lanes and len(indication) == 3:
+            try:
+                action = session.functionsList[indication[0]]
+                action(session)
+                programFlow(session)
+            except TypeError:
+                print('unknown syntax, please try again\n')
+                programFlow(session)
+    elif indication[0] in session.lanes and len(indication) == 3:
         print('Adding value ' + indication[1] + ' to lane ' + indication[0])
-        moneyInfo.attachData(indication[0], indication[1], indication[2])
-        programFlow()
-    elif len(indication) == 1 and indication[0].lower() == 'help':
-        print('help') #add help paths
-        programFlow()
-    elif len(indication) == 1 and indication[0].lower() == 'exit':
-        print('Ending session...')
-        exit()
+        session.attachData(indication[0], indication[1], indication[2])
+        programFlow(session)
     else:
         print('unknown syntax, please try again\n')
-        programFlow()
+        programFlow(session)
 
 def ingressPassword(user):
     queriedPassword = str(input('Ingress your password: '))
     if credentials.getPassword(queriedPassword, user):
         print('\nWelcome ' + str(user) + '\n')
         print('Fetching resources... \n')
-        programFlow()
+        session = moneyInfo.Session(user)
+        programFlow(session)
     else:
         print('user and password doesn\'t match... ')
         ingressPassword(user)
--- internalData/moneyInfo/__init__.py
+++ internalData/moneyInfo/__init__.py
@@ -1,54 +1,76 @@
 import json
 import math
 
-'''
-TODO:
-create a class container for all the methods that should be initialized
-once the user is confirmed to get the appropiate .json file
-'''
-outgoData = json.load(open('internalData/moneyInfo/uramirez.json', 'r'))
-
-def clear(lane):
-    outgoData[lane] = []
-    with open('internalData/moneyInfo/uramirez.json', 'w') as file:
-        json.dump(outgoData, file, ensure_ascii= False, indent= 4)
-
-def current(lane):
-    current, i = 0, 0
-    while i < len(outgoData[lane]):
-        current += outgoData[lane][i]["amount"]
-        i += 1
-    print('The total spent on this lane is: ' + str(current))
-
-def averages():
-    laneTotal, total = [], 0
-    for item in outgoData:
-        temp, i = 0, 0
-        while i < len(outgoData[item]):
-            total += outgoData[item][i]["amount"]
-            temp += outgoData[item][i]["amount"]
-            i += 1
-        laneTotal.append(temp)
-    indexer = 0
-    while indexer < len(laneTotal):
+class Session():
+    def __init__(self, user):
+        self.path = 'internalData/moneyInfo/DataBases/' + str(user) + '.json'
         try:
-            percentage = math.ceil(((laneTotal[indexer] * 100) / total) * 100) / 100
-            print('the lane {} represents the {} of the total of {}'.format(lanes[indexer], percentage, total))
-            indexer += 1
-        except ZeroDivisionError:
-            print('Math error, there is no values added.')
-            print('add some expenses and try again... ')
-            break
+            with open(self.path, 'r') as file:
+                self.outgoData = json.load(file)
+        except FileNotFoundError:
+            print('creating new database')
+            with open(self.path, 'w+') as file:
+                baseJson = {
+                    "travel": [],
+                    "lunchFood": [],
+                    "clubOutgo": [],
+                    "nightsOut": [],
+                    "games": [],
+                    "savedMoney": [],
+                    "others": [] }
+                json.dump(baseJson, file, ensure_ascii= False, indent= 4)
+            with open(self.path, 'r') as file:
+                self.outgoData = json.load(file)
+        self.lanes = ["travel", "lunchFood", "clubOutgo", "nightsOut", "games", "savedMoney", "others"]
 
-def attachData(typeOf, amount, description):
-    outgoData[typeOf].append({"amount": int(amount), "description": description})
-    with open('internalData/moneyInfo/uramirez.json', 'w') as file:
-        json.dump(outgoData, file, ensure_ascii= False, indent= 4)
+    def clear(self, lane):
+        self.outgoData[lane] = []
+        with open(self.path, 'w') as file:
+            json.dump(self.outgoData, file, ensure_ascii= False, indent= 4)
 
-functionsList = {
-    "clear": clear,
-    "current": current,
-    "average": averages
-}
+    def current(self, lane):
+        current, i = 0, 0
+        while i < len(self.outgoData[lane]):
+            current += self.outgoData[lane][i]["amount"]
+            i += 1
+        print('The total spent on this lane is: ' + str(current))
 
-lanes = ["travel", "lunchFood", "clubOutgo", "nightsOut", "games", "savedMoney", "others"]
+    def close(self):
+        print('Ending session... ')
+        exit()
+
+    def helpLines(self):
+        print('Help') # Format the help pages
+
+    def averages(self):
+        laneTotal, total = [], 0
+        for item in self.outgoData:
+            temp, i = 0, 0
+            while i < len(self.outgoData[item]):
+                total += self.outgoData[item][i]["amount"]
+                temp += self.outgoData[item][i]["amount"]
+                i += 1
+            laneTotal.append(temp)
+        indexer = 0
+        while indexer < len(laneTotal):
+            try:
+                percentage = math.ceil(((laneTotal[indexer] * 100) / total) * 100) / 100
+                print('the lane {} represents the {}% of the total of {}'.format(self.lanes[indexer], percentage, total))
+                indexer += 1
+            except ZeroDivisionError:
+                print('Math error, there is no values added.')
+                print('add some expenses and try again... ')
+                break
+
+    def attachData(self, typeOf, amount, description):
+        self.outgoData[typeOf].append({"amount": int(amount), "description": description})
+        with open(self.path, 'w') as file:
+            json.dump(self.outgoData, file, ensure_ascii= False, indent= 4)
+    
+    functionsList = {
+        "help": helpLines,
+        "exit": close,
+        "clear": clear,
+        "current": current,
+        "averages": averages
+    }
